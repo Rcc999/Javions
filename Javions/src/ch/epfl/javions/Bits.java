0c6bf2961cc63ddc40e3af214a9ed485b9028d9a
rcharif <rayane.charifchefchaouni@epfl.ch>
2023-02-21 20:07:55 +0000

    Bits class coded

--- Javions/src/ch/epfl/javions/Bits.java
+++ Javions/src/ch/epfl/javions/Bits.java
@@ -2,7 +2,29 @@ package ch.epfl.javions;
 
 public class Bits {
 
-    int extracUINt;
+    public static int extractUInt(long value, int start, int size){
 
+        if (size <= 0 || size > Integer.SIZE) {
+            throw new IllegalArgumentException("Invalid size: " + size);
+        }
+
+        // Ensure that the range described by start and size is within the bounds of the 64-bit value
+        if (start < 0 || start + size > Long.SIZE) {
+            throw new IndexOutOfBoundsException("Invalid range: start=" + start + ", size=" + size);
+        }
+
+        // Mask the relevant bits and shift them to the right position
+        long mask = (1L << size) - 1;
+        int shiftedValue = (int) ((value >> start) & mask);
+
+        return shiftedValue;
+    }
+
+    public static boolean testBit(long value, int index) {
+        if (index < 0 || index >= Long.SIZE) {
+            throw new IndexOutOfBoundsException("Index must be between 0 (inclusive) and 64 (exclusive)");
+        }
+        return ((value >>> index) & 1) == 1;
+    }
 
 }
