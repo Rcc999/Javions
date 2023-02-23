4c43ec93479448873846895e69acddf49c536be8
rcharif <rayane.charifchefchaouni@epfl.ch>
2023-02-23 18:11:13 +0000

    ByteStrings push

--- Javions/src/ch/epfl/javions/Bits.java
+++ Javions/src/ch/epfl/javions/Bits.java
@@ -2,14 +2,14 @@ package ch.epfl.javions;
 
 public class Bits {
 
+    private Bits (){};
+
     public static int extractUInt(long value, int start, int size){
 
-        if (size <= 0 || size > Integer.SIZE) {
-            throw new IllegalArgumentException("Invalid size: " + size);
-        }
+        Preconditions.checkArgument(size <= 0 || size > Integer.SIZE);
 
         // Ensure that the range described by start and size is within the bounds of the 64-bit value
-        if (start < 0 || start + size > Long.SIZE) {
+        if (start <= 0 || start + size > Long.SIZE) {
             throw new IndexOutOfBoundsException("Invalid range: start=" + start + ", size=" + size);
         }
 
@@ -21,7 +21,7 @@ public class Bits {
     }
 
     public static boolean testBit(long value, int index) {
-        if (index < 0 || index >= Long.SIZE) {
+        if (index <= 0 || index >= Long.SIZE) {
             throw new IndexOutOfBoundsException("Index must be between 0 (inclusive) and 64 (exclusive)");
         }
         return ((value >>> index) & 1) == 1;
--- Javions/src/ch/epfl/javions/ByteString.java
+++ Javions/src/ch/epfl/javions/ByteString.java
@@ -1,8 +1,10 @@
 package ch.epfl.javions;
 
+import java.util.Arrays;
 import java.util.HexFormat;
 
 public final class ByteString {
+
 
     private final byte[] bytes;
 
@@ -10,18 +12,27 @@ public final class ByteString {
         this.bytes = bytes;
     }
 
-    public static ByteString ofHexadecimalString(String hexString){
+    public static ByteString ofHexadecimalString(String hexString) {
 
         Preconditions.checkArgument(hexString.length() % 2 != 0);
 
-        HexFormat hf = HexFormat.of().withUpperCase();
+        //HexFormat hf = HexFormat.of().withUpperCase();
         //HexFormat hex = new HexFormat();
-        byte[] bytes = hf.parseHex(hexString);
+        //byte[] bytes = hf.parseHex(hexString);
+        HexFormat hf = HexFormat.of().withUpperCase();
+
+        byte[] bytes = new byte[]{(byte) 0x01, (byte) 0xAB};
+        String string = hf.formatHex(bytes); // vaut "01AB"
+        byte[] bytes2 = hf.parseHex(string); // identique à bytes
+        System.out.println(Arrays.equals(bytes, bytes2)); // true
+
         return new ByteString(bytes);
 
-        }
+    }
 
-        public int size(){return bytes.length;}
+    public int size() {
+        return bytes.length;
+    }
 
     public int byteAt(int index) {
 
@@ -32,14 +43,14 @@ public final class ByteString {
     }
 
 
-    public long bytesInRange(int fromIndex, int toIndex){
+    public long bytesInRange(int fromIndex, int toIndex) {
 
         if (fromIndex < 0 || toIndex > bytes.length || toIndex <= fromIndex) {
             throw new IndexOutOfBoundsException();
         }
-        if (toIndex - fromIndex > 8) {
-            throw new IllegalArgumentException();
-        }
+
+        Preconditions.checkArgument(toIndex - fromIndex > 8);
+
         long result = 0;
         for (int i = toIndex - 1; i >= fromIndex; i--) {
             result = (result << 8) | (bytes[i] & 0xff);
@@ -47,7 +58,20 @@ public final class ByteString {
         return result;
     }
 
+    public boolean equals(Object that0) {
+        if (that0 instanceof ByteString that && Arrays.equals(((ByteString) that0).bytes, that.bytes)) {
+            return true;
+        }
+        return false;
+    }
 
+    public int hashCode(){
+        return Arrays.hashCode(bytes);
+    }
 
+    public String toString(){
+        return HexFormat.of().wi
+    }
 
 }
+
