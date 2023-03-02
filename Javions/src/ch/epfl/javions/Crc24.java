2b55e0db108e2f07c6b5050945502220d71ee873
rcharif <rayane.charifchefchaouni@epfl.ch>
2023-03-02 15:21:06 +0100

    Crc24 new algo version

--- Javions/src/ch/epfl/javions/Crc24.java
+++ Javions/src/ch/epfl/javions/Crc24.java
@@ -5,6 +5,7 @@ public final class Crc24 {
     public static int GENERATOR = 0xFFF40916;
 
     private final int generator;
+    private int[] table;
 
     private static final int LENGTH_CRC24 = 24;
 
@@ -12,33 +13,42 @@ public final class Crc24 {
         this.generator = generator & 0xFFFFFF;
     }
 
-    /*private static int crc_bitwise(int generator, byte [] bytes){
+    private static int crc_bitwise(int generator, byte [] bytes){
+        /*int crc = 0;
+        int[] table = {0, generator};
+        for (byte b : bytes) {
+            for (int i = 0; i < Byte.SIZE ; i++) {
+                int bit = Bits.extractUInt(b, Byte.SIZE - 1 - i, 1);
+                int index = ((crc >> (LENGTH_CRC24 - 1)) ^ bit) & 0x1;
+                crc = ((crc << 1) | bit) ^ table[index];
+            }
+        }
+
+        return crc  & 0xFFFFFF;*/
+
+
         int crc = 0;
         int[] table = {0, generator};
-        for (int i = 0; i < bytes.length; i++) {
-            for (int j = 0; j < Byte.SIZE ; j++) {
-                int bitsExtracted = Bits.extractUInt(bytes[i], i, 1);
-                crc = ((crc << 1) | bitsExtracted) ^ table[crc[LENGTH_CRC24 - 1]];
 
-            }
-
-        }
-    }*/
-
-    private static int crc_bitwise(int generator, byte[] bytes) {
-        int crc = 0;
-        int[] table = {0, generator & 0xFFFFFF};
         for (byte b : bytes) {
-            for (int j = 0; j < Byte.SIZE; j++) {
-                int bit = Bits.extractUInt(b, j, 1);
-                crc = ((crc << 1) | bit) ^ table[crc >> (LENGTH_CRC24 - 1)];
+            for (int i = 0; i < Byte.SIZE ; i++) {
+                int bit = Bits.extractUInt(b, Byte.SIZE - 1 - i, 1);
+                int index = (crc >> (LENGTH_CRC24 - 1)) ^ bit;
+                crc = ((crc << 1) | bit) ^ table[index] ;
             }
         }
+
+        for (int i = 0; i < LENGTH_CRC24; i++) {
+            int bit = 0;
+            int index = (crc >> (LENGTH_CRC24 - 1)) ^ bit;
+            crc = ((crc << 1) | bit) ^ table[index];
+        }
+
         return crc & 0xFFFFFF;
     }
+
 
     public static int crc(byte[] bytes) {
         return crc_bitwise(GENERATOR, bytes);
     }
-
 }
--- Javions/test/ch/epfl/javions/TestCrc24.java
+++ Javions/test/ch/epfl/javions/TestCrc24.java
@@ -1,21 +1,25 @@
-package ch.epfl.test;
+package ch.epfl.javions;
 
 import ch.epfl.javions.Crc24;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HexFormat;
 
 public class TestCrc24 {
 
+    @Test
     void testCRC23(){
         Crc24 crc24 = new Crc24(Crc24.GENERATOR);
         String mS = "8D392AE499107FB5C00439";
         String cS = "035DB8";
         int c = Integer.parseInt(cS, 16); // == 0x035DB8
 
-       /* byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+        byte[] mAndC = HexFormat.of().parseHex(mS + cS);
         assertEquals(0, crc24.crc(mAndC));
 
         byte[] mOnly = HexFormat.of().parseHex(mS);
-        assertEquals(c, crc24.crc(mOnly));*/
+        assertEquals(c, crc24.crc(mOnly));
     }
 }
