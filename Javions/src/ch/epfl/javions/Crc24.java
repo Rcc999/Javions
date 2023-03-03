a532b7641d9b989a697310b57132bd263cfa56f6
Tuan <tuan.nguyen@epfl.ch>
2023-03-03 13:59:20 +0100

    Crc24 Update

--- Javions/src/ch/epfl/javions/Crc24.java
+++ Javions/src/ch/epfl/javions/Crc24.java
@@ -2,7 +2,7 @@ package ch.epfl.javions;
 
 public final class Crc24 {
 
-    public static int GENERATOR = 0xFFF40916;
+    public static final int GENERATOR = 0xFFF409;
 
     private final int generator;
     private int[] table;
@@ -11,6 +11,7 @@ public final class Crc24 {
 
     public Crc24(int generator){
         this.generator = generator & 0xFFFFFF;
+        //this.table = buildTable(generator);
     }
 
     private static int crc_bitwise(int generator, byte[] bytes){
@@ -19,20 +20,19 @@ public final class Crc24 {
         int[] table = {0, generator};
 
         for (byte b : bytes) {
-            for (int i = 0; i < Byte.SIZE ; i++) {
-                int bit = Bits.extractUInt(b, Byte.SIZE - 1 - i, 1);
-                int index = (crc >> (LENGTH_CRC24 - 1)) ^ bit;
+            for (int i = 7; i >= 0 ; i--) { // to check with assistant
+                int bit = Bits.extractUInt(b, i, 1);
+                int index = Bits.extractUInt(crc, LENGTH_CRC24 - 1, 1);
                 crc = ((crc << 1) | bit) ^ table[index] ;
             }
         }
 
         for (int i = 0; i < LENGTH_CRC24; i++) {
-            int bit = 0;
-            int index = (crc >> (LENGTH_CRC24 - 1)) ^ bit;
-            crc = ((crc << 1) | bit) ^ table[index];
+            int index = Bits.extractUInt(crc, LENGTH_CRC24 - 1, 1);
+            crc = (crc << 1) ^ table[index];
         }
 
-        return crc & 0xFFFFFF;
+        return Bits.extractUInt(crc, 0, LENGTH_CRC24); //crc & 0xFFFFFF
     }
 
     public static int crc(byte[] bytes) {
