1b8b17af8916562fb4bbbca511a97964386501cf
Tuan <tuan.nguyen@epfl.ch>
2023-03-24 14:07:39 +0100

    AirbornePositionMessageTestPersonal added

---
+++ Javions/test/ch/epfl/javions/adsb/AirbornePositionMessageTestPersonal.java
@@ -0,0 +1,27 @@
+package ch.epfl.javions.adsb;
+
+import ch.epfl.javions.demodulation.AdsbDemodulator;
+import ch.epfl.javions.demodulation.AdsbDemodulatorTestPersonal;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AirbornePositionMessageTestPersonal {
+    public static void main(String[] args) throws IOException {
+        String file = AdsbDemodulatorTestPersonal.class.getResource("/samples_20230304_1442.bin").getFile();
+        try (InputStream stream = new FileInputStream(file)) {
+            AdsbDemodulator demodulator = new AdsbDemodulator(stream);
+            RawMessage message;
+            int length = 0;
+            while ((message = demodulator.nextMessage()) != null) {
+                AirbornePositionMessage air = AirbornePositionMessage.of(message);
+                if(air != null){
+                    System.out.println(air);
+                    length++;
+                }
+            }
+            System.out.println(length);
+        }
+    }
+}
