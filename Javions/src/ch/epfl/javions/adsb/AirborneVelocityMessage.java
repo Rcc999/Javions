2b29c5326b88f7c3c92c93b86b2ae62214a8a480
rcharif <rayane.charifchefchaouni@epfl.ch>
2023-03-27 12:32:36 +0200

    AirborneVelocity pushs

--- Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -25,30 +25,52 @@ public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
     }
 
     private static double speedCalculator(RawMessage rawMessage){
-        int  subType =  Bits.extractUInt(rawMessage.payload(), 48, 3);
-        int Vew = Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 21, 22), 11, 10);
-        int Vns = Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 21, 22), 0, 10);
-        int AS = Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 21, 22), 0, 10);
-        double groundSpeed = Math.hypot(Vew - 1, Vns - 1);
+        double groundSpeed = groundSpeedCalculator(rawMessage);
         double groundSpeedInKnots = Units.convertFrom(groundSpeed, Units.Speed.KNOT );
-        if(Vew == 0 || Vns == 0){return 0;}
-        if(subType == 1 ){return groundSpeedInKnots; }
-        if(subType == 2){return 4 * groundSpeedInKnots;}
-        if(subType ==  3){return Units.convertFrom(AS, Units.Speed.KNOT );}
-        if(subType == 4){ return 4 * Units.convertFrom(AS, Units.Speed.KNOT );}
+        if(groundMovementCalculator(rawMessage)[2] == 0 || groundMovementCalculator(rawMessage)[4] == 0){return 0;}
+        if(subTypeCalculator(rawMessage) == 1 ){return groundSpeedInKnots; }
+        if(subTypeCalculator(rawMessage) == 2){return 4 * groundSpeedInKnots;}
+        if(subTypeCalculator(rawMessage) ==  3){return Units.convertFrom(airMovementCalculator(rawMessage)[3], Units.Speed.KNOT );}
+        if(subTypeCalculator(rawMessage) == 4){ return 4 * Units.convertFrom(airMovementCalculator(rawMessage)[3], Units.Speed.KNOT );}
+
         return 0;
     }
 
     private static double trackOrHeadingCalculator(RawMessage rawMessage){
-        int subType =  Bits.extractUInt(rawMessage.payload(), 48, 3);
-        int Dew = Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 21, 22), 21, 1);
-        int Dns = Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 21, 22), 10, 1);
-        int SH = Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 21, 22), 21, 1);
-        int HDG = Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 21, 22), 11, 10);
 
+        if(airMovementCalculator(rawMessage)[1] == 1){
+            double cap = (char) airMovementCalculator(rawMessage)[2] /  Math.pow(2, 10);
+            return Units.convertFrom(cap, Units.Angle.RADIAN);
+        }
         return 0;
     }
 
+    private static double groundSpeedCalculator(RawMessage rawMessage){
+        return Math.hypot(groundMovementCalculator(rawMessage)[2] - 1, groundMovementCalculator(rawMessage)[4] - 1);
+    }
+
+    private static int subTypeCalculator(RawMessage rawMessage){
+        return Bits.extractUInt(rawMessage.payload(), 48, 3);
+    }
+
+    private static int bitsInterpretedBySubType(RawMessage rawMessage){
+        return Bits.extractUInt(rawMessage.payload(), 21, 22);
+    }
+
+    private static int [] groundMovementCalculator(RawMessage rawMessage){
+        int Dew = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 21, 1);
+        int Vew = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 11, 10);
+        int Dns = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 10, 1);
+        int Vns = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 0, 10);
+        return new int[]{Dew, Vew, Dns, Vns};
+    }
+
+    private static int [] airMovementCalculator(RawMessage rawMessage){
+        int SH = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 21, 1);
+        int HDG = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 11, 10);
+        int AS = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 0, 10);
+        return new int [] {SH, HDG, AS};
+    }
 
     @Override
     public long timeStampNs() {
