744bda8b1ebf9ded12228393ebd47effaf1f11c8
Tuan <tuan.nguyen@epfl.ch>
2023-03-28 16:00:12 +0200

    AircraftStateAccumulator completed

--- Javions/src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
+++ Javions/src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
@@ -5,7 +5,7 @@ import ch.epfl.javions.GeoPos;
 public class AircraftStateAccumulator<T extends AircraftStateSetter> {
 
     private final T stateSetter;
-    private AirbornePositionMessage positionMessage;
+    private AirbornePositionMessage currentPositionMessage;
 
     public AircraftStateAccumulator(T stateSetter){
         if(stateSetter == null) throw new NullPointerException("State setter is null");
@@ -17,29 +17,53 @@ public class AircraftStateAccumulator<T extends AircraftStateSetter> {
     }
 
     public void update(Message message){
+        stateSetter.setLastMessageTimeStampNs(message.timeStampNs());
         switch (message){
-            stateSetter.setLastMessageTimeStampNs(message.timeStampNs());
+            
             case AircraftIdentificationMessage identification -> {
                 stateSetter.setCallSign(identification.callSign());
                 stateSetter.setCategory(identification.category());
             }
-
             case AirbornePositionMessage position -> {
-                positionMessage = (AirbornePositionMessage) message;
+                AirbornePositionMessage previousPositionMessage = currentPositionMessage;
+                currentPositionMessage = (AirbornePositionMessage) message;
                 stateSetter.setAltitude(position.altitude());
-                //Check timeStamps - 10 seconds
-                //Check Parity
-                stateSetter.setPosition();
+                if(previousPositionMessage.parity() != currentPositionMessage.parity()){
+                    if(timeStampNsDiff(currentPositionMessage.timeStampNs(), previousPositionMessage.timeStampNs())){
+                        stateSetter.setPosition(positionCalculator(currentPositionMessage, currentPositionMessage, (AirbornePositionMessage) message));
+                    }
+                }
             }
-
             case AirborneVelocityMessage velocity -> {
                 stateSetter.setVelocity(velocity.speed());
                 stateSetter.setTrackOrHeading(velocity.trackOrHeading());
             }
+            default -> throw new IllegalStateException("Unexpected value: " + message);
         }
 
     }
 
-    private boolean
+    private boolean timeStampNsDiff(long current, long previous){
+        return (current - previous) <= Math.pow(10, 10);
+    }
+
+    private GeoPos positionCalculator(AirbornePositionMessage positionMessage1, AirbornePositionMessage positionMessage2, AirbornePositionMessage message){
+        double x0 = 0.0, y0 = 0.0, x1 = 0.0, y1 = 0.0;
+        switch (positionMessage1.parity()){
+            case 0 -> {
+                x0 = positionMessage1.x();
+                y0 = positionMessage1.y();
+                x1 = positionMessage2.x();
+                y1 = positionMessage2.y();
+            }
+            case 1 -> {
+                x0 = positionMessage2.x();
+                y0 = positionMessage2.y();
+                x1 = positionMessage1.x();
+                y1 = positionMessage1.y();
+            }
+        }
+        return CprDecoder.decodePosition(x0, y0, x1, y1, message.parity());
+    }
 
 }
