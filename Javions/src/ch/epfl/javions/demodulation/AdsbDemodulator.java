8e1f7f80e89858adacc1ab841b2a6356e3ceef23
Tuan <tuan.nguyen@epfl.ch>
2023-03-31 16:48:27 +0200

    AdsbDemodulator comments added

--- Javions/src/ch/epfl/javions/demodulation/AdsbDemodulator.java
+++ Javions/src/ch/epfl/javions/demodulation/AdsbDemodulator.java
@@ -1,72 +1,69 @@
 package ch.epfl.javions.demodulation;
 
 import ch.epfl.javions.adsb.RawMessage;
-import ch.epfl.javions.demodulation.PowerWindow;
-
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Demodulation of ADS-B message
+ *
+ * @author Tuan Dang Nguyen (361089)
+ * @author Rayane Charif Chefchouni (339839)
+ */
 public final class AdsbDemodulator {
 
     private final PowerWindow powerWindow;
     private final static  int WINDOW_SIZE = 1200;
     private int sumActualWindow = 0;
-    private int v;
-    private int sumNextWindow = 0;
-    private int sumPreviousWindow = 0;
-    private byte[] message;
 
-
+    /**
+     * Construct a window over the samples data
+     *
+     * @param samplesStream contains samples data
+     * @throws IOException if there is an input/output error from the samples stream
+     */
     public AdsbDemodulator(InputStream samplesStream) throws IOException{
         powerWindow = new PowerWindow(samplesStream, WINDOW_SIZE);
     }
 
+    /**
+     * Return a raw ADS-B message from samples data
+     *
+     * @return valid raw message obtained after demodulating the samples data
+     * @throws IOException if there is an input/output error from the samples stream
+     */
     public RawMessage nextMessage() throws IOException{
+
         while(powerWindow.isFull()) {
-            //System.out.println("In loop");
-            message = new byte[14];
-            sumPreviousWindow = sumActualWindow;
+            byte[] message = new byte[14];
+            int sumPreviousWindow = sumActualWindow;
             sumActualWindow = powerWindow.get(0) + powerWindow.get(10) + powerWindow.get(35) + powerWindow.get(45);
-            sumNextWindow = powerWindow.get(1) + powerWindow.get(11) + powerWindow.get(36) + powerWindow.get(46);
-            v = powerWindow.get(5) + powerWindow.get(15) + powerWindow.get(20) + powerWindow.get(25) + powerWindow.get(30) + powerWindow.get(40);
+            int sumNextWindow = powerWindow.get(1) + powerWindow.get(11) + powerWindow.get(36) + powerWindow.get(46);
+            int v = powerWindow.get(5) + powerWindow.get(15) + powerWindow.get(20) + powerWindow.get(25) + powerWindow.get(30) + powerWindow.get(40);
             int diff_2 = sumActualWindow - sumNextWindow;
             int diff_1 = sumActualWindow - sumPreviousWindow;
 
-            // Check if the preamble is there with the different between the previous - actual and next - actual
             if((diff_1 > 0) && (diff_2 > 0) && (sumActualWindow >= 2 * v) ){
                 // Decode to get the message and put each byte (8 bits) in the table
                 for(int i = 0; i < message.length; ++i){
                     int a = 0;
                     for(int j = 0; j < 8; ++j){
                         if(powerWindow.get(80 + 10 * (j + i * 8)) < powerWindow.get(85 + 10 * (j + i * 8))){
-                            a = (a << 1); //tableau de byte, concartener chaque 8 bits
-                            //System.out.println( "i = " + i + ", j = " + j  + " Binary = " + Integer.toBinaryString(a));
+                            a = (a << 1);
                         }else{
                             a = ((a << 1) | 1);
-                            //System.out.println("i = " + i + ", j = " + j  + " Binary = " + Integer.toBinaryString(a));
                         }
                     }
                     message[i] = (byte) a;
                 }
-                //System.out.println(powerWindow.position());
                 RawMessage rawMessage = RawMessage.of(powerWindow.position() * 100, message);
-
 
                 if((RawMessage.size(message[0]) == RawMessage.LENGTH) && rawMessage != null){
                     powerWindow.advanceBy(WINDOW_SIZE);
-                    /*for (int i = 0; i < message.length; i++) {
-                        System.out.print(message[i] + ", ");
-                    }*/
                     return rawMessage;
-                } else {
-                    powerWindow.advance();
-                }
-            } else {
-                powerWindow.advance();
-            }
+                } else powerWindow.advance();
+            } else powerWindow.advance();
         }
-
         return null;
     }
-
 }
