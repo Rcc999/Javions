2acc15e0ab83a580bc71f369a9d41d2dca3f4db2
rcharif <rayane.charifchefchaouni@epfl.ch>
2023-03-31 13:57:54 +0200

    AirborneVelocity push with errors to correct

--- Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -13,30 +13,26 @@ public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
     }
 
     public static AirborneVelocityMessage of(RawMessage rawMessage){
-        if (rawMessage.typeCode() ==  19){
-            if(speedCalculator(rawMessage, subTypeCalculator(rawMessage)) ==  0) {return null;}
-            return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speedCalculator(rawMessage, subTypeCalculator(rawMessage)), trackOrHeadingCalculator(rawMessage));
-        }
-
-
-        return null;
-
+        if(Double.isNaN(speedCalculator(rawMessage, subTypeCalculator(rawMessage))) || Double.isNaN(trackOrHeadingCalculator(rawMessage, subTypeCalculator(rawMessage)))) { return null; }
+        return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speedCalculator(rawMessage, subTypeCalculator(rawMessage)), trackOrHeadingCalculator(rawMessage, subTypeCalculator(rawMessage)));
     }
 
     private static double speedCalculator(RawMessage rawMessage, int subType){
         return switch (subType) {
             case 1, 2 -> groundSpeedCalculator(rawMessage);
             case 3, 4 -> airSpeedCalculator(rawMessage);
-            default -> 0;
+            default -> Double.NaN;
         };
     }
 
 
-
-    private static double trackOrHeadingCalculator(RawMessage rawMessage){
-     return Math.atan2(groundHeadingNorthSouthCalculator(rawMessage), groundHeadingEastWestCalculator(rawMessage));
+    private static double trackOrHeadingCalculator(RawMessage rawMessage, int subType){
+        return switch (subType) {
+            case 1, 2 -> trackOrHeadingGroundCalculator(rawMessage);
+            case 3, 4 -> trackOrHeadingAirCalculator(rawMessage);
+            default -> Double.NaN;
+        };
     }
-
 
 
     private static int subTypeCalculator(RawMessage rawMessage){
@@ -60,21 +56,25 @@ public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
     }
 
     private static double groundSpeedCalculator(RawMessage rawMessage){
-        if(groundMovementCalculator(rawMessage)[1] == 0 || groundMovementCalculator(rawMessage)[3] == -1){return 0;}
+        if(groundMovementCalculator(rawMessage)[1] == -1 || groundMovementCalculator(rawMessage)[3] == -1){ return Double.NaN; }
         if(subTypeCalculator(rawMessage) == 1 ){return Units.convertFrom(groundSpeedNormalized(rawMessage), Units.Speed.KNOT); }
         if(subTypeCalculator(rawMessage) == 2){return 4 * Units.convertFrom(groundSpeedNormalized(rawMessage), Units.Speed.KNOT);}
-        return 0;
+        return Double.NaN;
     }
 
     private static double groundHeadingEastWestCalculator(RawMessage rawMessage){
-        return groundMovementCalculator(rawMessage)[1] == 0 ? groundSpeedNormalized(rawMessage) : -groundSpeedNormalized(rawMessage);
+        return groundMovementCalculator(rawMessage)[0] == 0 ? groundSpeedNormalized(rawMessage) : -groundSpeedNormalized(rawMessage);
     }
 
 
     private static double groundHeadingNorthSouthCalculator(RawMessage rawMessage){
-        return groundMovementCalculator(rawMessage) [3] == 0 ? groundSpeedNormalized(rawMessage) : -groundSpeedNormalized(rawMessage);
+        return groundMovementCalculator(rawMessage) [2] == 0 ? groundSpeedNormalized(rawMessage) : -groundSpeedNormalized(rawMessage);
     }
 
+    private static double trackOrHeadingGroundCalculator(RawMessage rawMessage){
+        double trackOrHeadingGround =  Math.atan2(groundHeadingNorthSouthCalculator(rawMessage), groundHeadingEastWestCalculator(rawMessage));
+        return trackOrHeadingGround < 0 ? trackOrHeadingGround + Units.Angle.TURN : trackOrHeadingGround;
+    }
 
     private static int [] airMovementCalculator(RawMessage rawMessage){
         int SH = Bits.extractUInt(bitsInterpretedBySubType(rawMessage), 21, 1);
@@ -84,16 +84,16 @@ public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
     }
 
     private static double airSpeedCalculator(RawMessage rawMessage){
-        if(airMovementCalculator(rawMessage)[3] == -1){ return 0;}
+        if(airMovementCalculator(rawMessage)[2] == -1){ return Double.NaN;}
         if(subTypeCalculator(rawMessage) ==  3){return Units.convertFrom(airMovementCalculator(rawMessage)[2], Units.Speed.KNOT );}
         if(subTypeCalculator(rawMessage) == 4){ return 4 * Units.convertFrom(airMovementCalculator(rawMessage)[2], Units.Speed.KNOT );}
-        return 0;
+        return Double.NaN ;
     }
 
 
-    private static double airHeadingCalculator(RawMessage rawMessage){
+    private static double trackOrHeadingAirCalculator(RawMessage rawMessage){
         if(airMovementCalculator(rawMessage)[0] == 1){ return Units.convertFrom(Math.scalb(airMovementCalculator(rawMessage)[1], -10), Units.Angle.RADIAN);}
-        return 0;
+        return Double.NaN;
     }
 
     @Override
