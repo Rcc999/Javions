870c62fdd0f5c321734ea3999faa5bbc50aa2d76
Tuan <tuan.nguyen@epfl.ch>
2023-04-04 09:38:27 +0200

    AircraftData comments added

--- Javions/src/ch/epfl/javions/aircraft/AircraftData.java
+++ Javions/src/ch/epfl/javions/aircraft/AircraftData.java
@@ -2,9 +2,26 @@ package ch.epfl.javions.aircraft;
 
 import java.util.Objects;
 
+/**
+ * Checking data of an aircraft
+ *
+ * @author Tuan Dang Nguyen (361089)
+ * @author Rayane Charif Chefchouni (339839)
+ */
 public record AircraftData(AircraftRegistration registration, AircraftTypeDesignator typeDesignator,
                            String model, AircraftDescription description, WakeTurbulenceCategory wakeTurbulenceCategory) {
 
+    /**
+     * Collect fixed data of an aircraft
+     *
+     * @param registration
+     * @param typeDesignator
+     * @param model
+     * @param description
+     * @param wakeTurbulenceCategory
+     *
+     * @throws NullPointerException if one of the parameter is null
+     */
     public AircraftData{
         Objects.requireNonNull(registration);
         Objects.requireNonNull(typeDesignator);
