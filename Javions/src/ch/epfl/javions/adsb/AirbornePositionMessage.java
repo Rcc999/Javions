aaa021da55e449087fb2a6f94407ba7e09be9d47
Tuan <tuan.nguyen@epfl.ch>
2023-04-04 14:40:33 +0200

    Comments updated

--- Javions/src/ch/epfl/javions/adsb/AirbornePositionMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AirbornePositionMessage.java
@@ -5,52 +5,112 @@ import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.Units;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
+/**
+ * Position message of an aircraft
+ *
+ * @author Tuan Dang Nguyen (361089)
+ * @author Rayane Charif Chefchouni (339839)
+ */
 public record AirbornePositionMessage(long timeStampNs, IcaoAddress icaoAddress, double altitude, int parity,
-                                      double x, double y) implements Message{
+                                      double x, double y) implements Message {
 
-
+    /**
+     * Construct an airborne position message
+     *
+     * @param timeStampNs of the aircraft
+     * @param icaoAddress of the aircraft
+     * @param altitude    of the aircraft
+     * @param parity      of message
+     * @param x           : local longitude normalized
+     * @param y           : local latitude normalized
+     * @throws NullPointerException     if the ICAO address of the aircraft is null
+     * @throws IllegalArgumentException if time stamps is negative or the parity isn't 0 nor 1
+     * @throws IllegalArgumentException if the value of local longitude and latitude aren't between 0 (included) and 1 (excluded)
+     */
     public AirbornePositionMessage {
-        if (icaoAddress == null) { throw new NullPointerException("IcaoAddress is null");}
+        if (icaoAddress == null) {
+            throw new NullPointerException("IcaoAddress is null");
+        }
         Preconditions.checkArgument(timeStampNs >= 0);
         Preconditions.checkArgument(parity == 0 || parity == 1);
         Preconditions.checkArgument(x >= 0 && x < 1);
         Preconditions.checkArgument(y >= 0 && y < 1);
     }
+
+    /**
+     * Get a position message of the aircraft
+     *
+     * @param rawMessage of the aircraft
+     * @return position message of the aircraft or null if the message is invalid
+     */
+    public static AirbornePositionMessage of(RawMessage rawMessage) {
+        return Double.isNaN(altitudeCalculator(rawMessage)) ?
+                null : new AirbornePositionMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(),
+                altitudeCalculator(rawMessage), determineParity(rawMessage), LAT_CPR(rawMessage), LON_CPR(rawMessage));
+    }
+
+    /**
+     * Get the time stamp of the current position message
+     *
+     * @return time stamp of the current position message
+     */
     @Override
     public long timeStampNs() {
         return timeStampNs;
     }
 
+    /**
+     * Get the ICAO address of the current position message
+     *
+     * @return ICAO address of the current position message
+     */
     @Override
-    public IcaoAddress icaoAddress() {return icaoAddress;}
-
-    public static AirbornePositionMessage of(RawMessage rawMessage){
-        if(Double.isNaN(altitudeCalculator(rawMessage))){
-            return null;
-        }else{
-            return new AirbornePositionMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(),
-                    altitudeCalculator(rawMessage), determineParity(rawMessage), LAT_CPR(rawMessage), LON_CPR(rawMessage));
-        }
+    public IcaoAddress icaoAddress() {
+        return icaoAddress;
     }
 
-    private static double LAT_CPR(RawMessage rawMessage){
+    /**
+     * Calculate and normalize latitude from a raw message of the aircraft
+     *
+     * @param rawMessage of the aircraft
+     * @return latitude normalized extracted from raw message
+     */
+    private static double LAT_CPR(RawMessage rawMessage) {
         return Math.scalb(Bits.extractUInt(rawMessage.payload(), 0, 17), -17);
     }
 
-    private static double LON_CPR(RawMessage rawMessage){
+    /**
+     * Calculate and normalize longitude from raw message of the aircraft
+     *
+     * @param rawMessage of the aircraft
+     * @return longitude normalized extracted
+     */
+    private static double LON_CPR(RawMessage rawMessage) {
         return Math.scalb(Bits.extractUInt(rawMessage.payload(), 17, 17), -17);
     }
 
-    private static int determineParity(RawMessage rawMessage){
+    /**
+     * Calculate parity from raw message of the aircraft
+     *
+     * @param rawMessage of the aircraft
+     * @return the parity extracted
+     */
+    private static int determineParity(RawMessage rawMessage) {
         return Bits.extractUInt(rawMessage.payload(), 34, 1);
     }
 
-    private static double altitudeCalculator(RawMessage rawMessage){
+    /**
+     * Calculate altitude of the aircraft
+     *
+     * @param rawMessage of the aircraft
+     * @return altitude of the aircraft
+     */
+    private static double altitudeCalculator(RawMessage rawMessage) {
         int alt = Bits.extractUInt(rawMessage.payload(), 36, 12);
 
-        if(determineQ(rawMessage) == 1){
+        if (determineQ(rawMessage) == 1) {
             return Units.convert((double) -1000 + removeBitForQ1(alt) * 25, Units.Length.FOOT, Units.Length.METER);
-       } else {
+        } else {
             //Un-scramble
             alt = unscrambled(alt);
 
@@ -62,13 +122,13 @@ public record AirbornePositionMessage(long timeStampNs, IcaoAddress icaoAddress,
             group1 = GrayToValue(group1, 3);
             group2 = GrayToValue(group2, 9);
 
-            if(group1 == 0 || group1 == 5 || group1 == 6){
+            if (group1 == 0 || group1 == 5 || group1 == 6) {
                 return Double.NaN;
             }
-            if(group1 == 7){
+            if (group1 == 7) {
                 group1 = 5;
             }
-            if(group2 % 2 == 1){
+            if (group2 % 2 == 1) {
                 group1 = 6 - group1;
             }
 
@@ -76,31 +136,35 @@ public record AirbornePositionMessage(long timeStampNs, IcaoAddress icaoAddress,
         }
     }
 
-    private static int determineQ(RawMessage rawMessage){
-        return Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 36, 12) , 4, 1);
+    /**
+     * Determine bit Q (in order to calculate the altitude)
+     *
+     * @param rawMessage of the aircraft
+     * @return value of bit Q of the aircraft
+     */
+    private static int determineQ(RawMessage rawMessage) {
+        return Bits.extractUInt(Bits.extractUInt(rawMessage.payload(), 36, 12), 4, 1);
     }
 
+    /**
+     * Calculate the value of the 12 bits of attribute ALT when Q = 1
+     *
+     * @param num : attribute ALT extracted from raw message of the aircraft
+     * @return the value of attribute ALT upon removing bit Q
+     */
     private static int removeBitForQ1(int num) {
         int mask = (1 << 4) - 1;
         return (char) ((num & ((~mask) << 1)) >>> 1) | (num & mask);
     }
 
-    private static int shouldBeReplaceWithBetterAlgo(int b){
-        int[] a = new int[12];
-        for(int i = 0; i < 12; ++i){
-            a[i] = Bits.extractUInt(b, i, 1);
-        }
-
-        int[] c = {a[4], a[2], a[0], a[10], a[8], a[6], a[5], a[3], a[1], a[11], a[9], a[7]};
-
-        int d = 0;
-        for (int j : c) {
-            d = (d << 1) | j;
-        }
-        return d;
-    }
-
-    private static int unscrambled(int b){
+    /**
+     * Unscramble the 12 bits of attribute ALT
+     * An operation in order to calculate the altitude of the aircraft when Q = 0
+     *
+     * @param b : attribute ALT extracted from raw message of the aircraft
+     * @return the unscrambled
+     */
+    private static int unscrambled(int b) {
         int swapped = 0;
         int[] bitOrder = {7, 9, 11, 1, 3, 5, 6, 8, 10, 0, 2, 4};
         for (int i = 0; i < bitOrder.length; i++) {
@@ -111,29 +175,20 @@ public record AirbornePositionMessage(long timeStampNs, IcaoAddress icaoAddress,
         return swapped;
     }
 
-    private static int GrayToValue(int gray, int nb_bits){
+    /**
+     * Decode Gray's code
+     *
+     * @param gray's  code
+     * @param nb_bits : number of bits that will be used to decode gray's code
+     * @return the decoded bits from Gray's code
+     */
+    private static int GrayToValue(int gray, int nb_bits) {
         int a = gray;
-        for(int i = 1; i < nb_bits; ++i){
+        for (int i = 1; i < nb_bits; ++i) {
             a = a ^ (gray >> i);
         }
         return a;
     }
-
-    /**
-    public static void main(String[] args) {
-        System.out.println(removeBitForQ1( 0b100010110011));
-        System.out.println(GrayToValue( 0b000101011, 9));
-        System.out.println(GrayToValue( 0b010, 3));
-        System.out.println(shouldBeReplaceWithBetterAlgo(0b011001001010));
-        System.out.println(Integer.toBinaryString(shouldBeReplaceWithBetterAlgo(0b011001001010)));
-        System.out.println(unscrambled(0b011001001010));
-        System.out.println(Integer.toBinaryString(unscrambled(0b011001001010)));
-
-        byte[] a = {-115, 73, 82, -103, 88, -77, 2, -26, -31, 95, -93, 82, 48, 107};
-        RawMessage rawMessage = RawMessage.of(75898000, a);
-        AirbornePositionMessage air = AirbornePositionMessage.of(rawMessage);
-        System.out.println(air);
-    }*/
 
 }
 
