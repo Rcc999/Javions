6cd5e1e9bd0fd2aa935822229497b431bdc73f5b
Tuan <tuan.nguyen@epfl.ch>
2023-04-21 14:16:54 +0200

    ObservableAircraftState updated

--- Javions/src/ch/epfl/javions/gui/ObservableAircraftState.java
+++ Javions/src/ch/epfl/javions/gui/ObservableAircraftState.java
@@ -5,20 +5,28 @@ import ch.epfl.javions.adsb.AircraftStateSetter;
 import ch.epfl.javions.adsb.CallSign;
 import ch.epfl.javions.aircraft.AircraftData;
 import ch.epfl.javions.aircraft.IcaoAddress;
-import javafx.beans.property.LongProperty;
-import javafx.beans.property.ReadOnlyLongProperty;
-import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.*;
 
 public final class ObservableAircraftState implements AircraftStateSetter {
 
     private final IcaoAddress icaoAddress;
     private final AircraftData data;
     private final LongProperty timeStampNsProperty;
+    private final IntegerProperty categoryProperty;
+    private final ObjectProperty<CallSign> callSignProperty;
+    private final DoubleProperty altitudeProperty, velocityProperty, trackOrHeadingProperty;
+    private final ObjectProperty<GeoPos> positionProperty;
 
     public ObservableAircraftState(IcaoAddress icaoAddress, AircraftData data){
         this.icaoAddress = icaoAddress;
         this.data = data;
         this.timeStampNsProperty = new SimpleLongProperty(0L);
+        this.categoryProperty = new SimpleIntegerProperty(0);
+        this.callSignProperty = new SimpleObjectProperty<>(null);
+        this.positionProperty = new SimpleObjectProperty<>(null);
+        this.altitudeProperty = new SimpleDoubleProperty(0.0);
+        this.velocityProperty = new SimpleDoubleProperty(0.0);
+        this.trackOrHeadingProperty = new SimpleDoubleProperty(0.0);
     }
 
     @Override
@@ -32,31 +40,55 @@ public final class ObservableAircraftState implements AircraftStateSetter {
 
     @Override
     public void setCategory(int category) {
+        categoryProperty.set(category);
+    }
 
+    public ReadOnlyIntegerProperty categoryProperty(){
+        return categoryProperty;
     }
 
     @Override
     public void setCallSign(CallSign callSign) {
+        callSignProperty.set(callSign);
+    }
 
+    public ReadOnlyObjectProperty<CallSign> callSignProperty(){
+        return callSignProperty;
     }
 
     @Override
     public void setPosition(GeoPos position) {
+        positionProperty.set(position);
+    }
 
+    public ReadOnlyObjectProperty<GeoPos> positionProperty(){
+        return positionProperty;
     }
 
     @Override
     public void setAltitude(double altitude) {
+        altitudeProperty.set(altitude);
+    }
 
+    public ReadOnlyDoubleProperty altitudeProperty(){
+        return altitudeProperty;
     }
 
     @Override
     public void setVelocity(double velocity) {
+        velocityProperty.add(velocity);
+    }
 
+    public ReadOnlyDoubleProperty velocityProperty(){
+        return velocityProperty;
     }
 
     @Override
     public void setTrackOrHeading(double trackOrHeading) {
+        trackOrHeadingProperty.set(trackOrHeading);
+    }
 
+    public ReadOnlyDoubleProperty trackOrHeadingProperty(){
+        return trackOrHeadingProperty;
     }
 }
