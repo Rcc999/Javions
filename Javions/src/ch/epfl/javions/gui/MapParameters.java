3ca1e747e4ba7ba43b08f3701a89e3823fee46eb
rcharif <rayane.charifchefchaouni@epfl.ch>
2023-04-25 09:34:43 +0200

    MapParameters push

--- Javions/src/ch/epfl/javions/gui/MapParameters.java
+++ Javions/src/ch/epfl/javions/gui/MapParameters.java
@@ -1,4 +1,16 @@
 package ch.epfl.javions.gui;
 
+import ch.epfl.javions.WebMercator;
+
 public final class MapParameters {
+
+private final int zoomLevel;
+    private final double x;
+    private final double y;
+
+    public MapParameters(int zoomLevel, double x, double y) {
+        this.zoomLevel = zoomLevel;
+        this.x = x;
+        this.y = y;
+    }
 }
