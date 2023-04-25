1e18197b710466d97f0311823b52ee27d3779d03
Tuan <tuan.nguyen@epfl.ch>
2023-04-25 14:39:28 +0200

    MapParameters updated

--- Javions/src/ch/epfl/javions/gui/MapParameters.java
+++ Javions/src/ch/epfl/javions/gui/MapParameters.java
@@ -1,16 +1,59 @@
 package ch.epfl.javions.gui;
 
+import ch.epfl.javions.Math2;
+import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.WebMercator;
+import javafx.beans.property.*;
 
 public final class MapParameters {
 
-private final int zoomLevel;
-    private final double x;
-    private final double y;
+    private final DoubleProperty minXProperty, minYProperty;
+    private final IntegerProperty zoomLevelProperty;
 
     public MapParameters(int zoomLevel, double x, double y) {
-        this.zoomLevel = zoomLevel;
-        this.x = x;
-        this.y = y;
+        Preconditions.checkArgument(zoomLevel >= 6 && zoomLevel <= 19);
+
+        this.zoomLevelProperty = new SimpleIntegerProperty(zoomLevel);
+        this.minXProperty = new SimpleDoubleProperty(x);
+        this.minYProperty = new SimpleDoubleProperty(y);
     }
+
+    public ReadOnlyDoubleProperty minXProperty(){
+        return minXProperty;
+    }
+
+    public double getMinX(){
+        return minXProperty.get();
+    }
+
+    public ReadOnlyDoubleProperty minYProperty(){
+        return minYProperty;
+    }
+
+    public double getMinY(){
+        return minYProperty.get();
+    }
+
+    public ReadOnlyIntegerProperty zoomLevelProperty(){
+        return zoomLevelProperty;
+    }
+
+    public int getZoomLevel(){
+        return zoomLevelProperty.get();
+    }
+
+    public void scroll(int x, int y){
+        minYProperty.set(getMinY() + y);
+        minXProperty.set(getMinX() + x);
+    }
+
+    public void changeZoomLevel(int zoomDiff){
+        int newZoomLevel = getZoomLevel() + zoomDiff;
+        Preconditions.checkArgument(Math2.clamp(6, newZoomLevel, 19) == newZoomLevel);
+        zoomLevelProperty.set(newZoomLevel);
+        minYProperty.set(getMinY() * Math.pow(2,newZoomLevel));
+        minXProperty.set(getMinX() * Math.pow(2,newZoomLevel));
+    }
+
+
 }
