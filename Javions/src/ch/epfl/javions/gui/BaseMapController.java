667ac1b66d54e99402f2fea22a5359598d47da51
rcharif <rayane.charifchefchaouni@epfl.ch>
2023-04-25 09:32:04 +0200

    Two last CLAss

---
+++ Javions/src/ch/epfl/javions/gui/BaseMapController.java
@@ -0,0 +1,11 @@
+package ch.epfl.javions.gui;
+
+public final class BaseMapController {
+
+    private final TileManager tileManager;
+
+
+    public BaseMapController(TileManager tileManager) {
+        this.tileManager = tileManager;
+    }
+}
