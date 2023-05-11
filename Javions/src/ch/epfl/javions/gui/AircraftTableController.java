7c5c8c577329083909cbbbd40045712ffcbe89eb
Tuan <tuan.nguyen@epfl.ch>
2023-05-11 14:27:42 +0200

    AircraftTableController updated

--- Javions/src/ch/epfl/javions/gui/AircraftTableController.java
+++ Javions/src/ch/epfl/javions/gui/AircraftTableController.java
@@ -126,7 +126,7 @@ public final class AircraftTableController {
     }
 
     private void setTableNumericalColumn(){
-        //Haven't taken into account cases where stuffs are 0
+        //Haven't taken into account cases where stuffs are 0: SOLVED
         //Number behind comma doesn't work yet: SOLVED
 
 
